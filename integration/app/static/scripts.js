f08bf5b7ebde90c318ce38d905d8fa9dd4382ecd
rcarlini <roberto.carlini@upf.edu>
2020-04-14 13:31:53 +0200

    Added fix for AudioContext in Chrome

--- integration/app/static/scripts.js
+++ integration/app/static/scripts.js
@@ -47,41 +47,42 @@ window.addEventListener('load', (event) => {
   function startRecording() {
 
     console.log("startButton clicked");
+    audioContext.resume().then( function() {
+      var constraints = {
+        audio: true,
+        video: false
+      }
+      /* Disable the record button until we get a success or fail from getUserMedia() */
 
-    var constraints = {
-      audio: true,
-      video: false
-    }
-    /* Disable the record button until we get a success or fail from getUserMedia() */
+      startButton.disabled = true;
+      stopButton.disabled = false;
+      sendButton.disabled = false
 
-    startButton.disabled = true;
-    stopButton.disabled = false;
-    sendButton.disabled = false
+      /* We're using the standard promise based getUserMedia()
+      
+      https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia */
 
-    /* We're using the standard promise based getUserMedia()
-    
-    https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia */
+      navigator.mediaDevices.getUserMedia(constraints)
+        .then(function (stream) {
+          console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
+          /* assign to gumStream for later use */
+          gumStream = stream;
+          /* use the stream */
+          input = audioContext.createMediaStreamSource(stream);
+          /* Create the Recorder object and configure to record mono sound (1 channel) Recording 2 channels will double the file size */
+          rec = new Recorder(input, {
+            numChannels: 1
+          })
+          //start the recording process 
+          rec.record()
+          console.log("Recording started");
 
-    navigator.mediaDevices.getUserMedia(constraints)
-      .then(function (stream) {
-        console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
-        /* assign to gumStream for later use */
-        gumStream = stream;
-        /* use the stream */
-        input = audioContext.createMediaStreamSource(stream);
-        /* Create the Recorder object and configure to record mono sound (1 channel) Recording 2 channels will double the file size */
-        rec = new Recorder(input, {
-          numChannels: 1
-        })
-        //start the recording process 
-        rec.record()
-        console.log("Recording started");
-
-      }).catch(function (err) {
-        //enable the record button if getUserMedia() fails 
-        startButton.disabled = false;
-        stopButton.disabled = true;
-        sendButton.disabled = true
+        }).catch(function (err) {
+          //enable the record button if getUserMedia() fails 
+          startButton.disabled = false;
+          stopButton.disabled = true;
+          sendButton.disabled = true
+        });
       });
   }
 
